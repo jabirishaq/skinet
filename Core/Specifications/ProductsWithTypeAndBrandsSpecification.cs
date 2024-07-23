367b9aeb18893afae5cbdbde3b0f75221060810f
punjab-zameen\jabir.ishaq <jabir.ishaq@punjab-zameen.gov.pk>
2024-07-23 11:46:00 +0500

    Search mechanism with pagination tested

--- Core/Specifications/ProductsWithTypeAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypeAndBrandsSpecification.cs
@@ -11,13 +11,13 @@ namespace Core.Specifications
     {   
         public ProductsWithTypeAndBrandsSpecification(ProductSpecParams productParams) 
             : base(x => (true) && 
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
             (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
             // Default criteria
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand); 
-            //AddOrderBy(x => x.Name);
             ApplyPaging(productParams.PageSize * (productParams.PageIndex -1), productParams.PageSize);
 
             if (!string.IsNullOrEmpty(productParams.Sort))
